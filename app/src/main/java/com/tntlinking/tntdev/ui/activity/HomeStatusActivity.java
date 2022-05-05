aa3b3321fb9dde31450be11006747ee8f854458a
DESKTOP-F2M2QUP\Administrator <18827060481@163.com>
2022-05-05 17:37:37 +0800

    首页添加添加职位

--- app/src/main/java/com/tntlinking/tntdev/ui/activity/HomeStatusActivity.java
+++ app/src/main/java/com/tntlinking/tntdev/ui/activity/HomeStatusActivity.java
@@ -13,21 +13,18 @@ import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
-import com.blankj.utilcode.constant.TimeConstants;
 import com.blankj.utilcode.util.SPUtils;
-import com.blankj.utilcode.util.TimeUtils;
+import com.google.android.material.tabs.TabLayout;
+import com.google.android.material.tabs.TabLayoutMediator;
 import com.gyf.immersionbar.ImmersionBar;
 import com.hjq.base.BaseDialog;
 import com.hjq.http.EasyHttp;
-import com.hjq.http.EasyLog;
 import com.hjq.http.listener.HttpCallback;
 import com.tntlinking.tntdev.R;
 import com.tntlinking.tntdev.app.AppActivity;
 import com.tntlinking.tntdev.http.api.AppListApi;
 import com.tntlinking.tntdev.http.api.AppListInterviewApi;
 import com.tntlinking.tntdev.http.api.GetAppUpdateApi;
-import com.tntlinking.tntdev.http.api.GetCodeApi;
-import com.tntlinking.tntdev.http.api.GetDeveloperStatusApi;
 import com.tntlinking.tntdev.http.api.GetNewbieApi;
 import com.tntlinking.tntdev.http.api.HistoryListApi;
 import com.tntlinking.tntdev.http.glide.GlideApp;
@@ -41,13 +38,19 @@ import com.tntlinking.tntdev.ui.adapter.HomeTaskAdapter;
 import com.tntlinking.tntdev.ui.adapter.ServiceProjectAdapter;
 import com.tntlinking.tntdev.ui.bean.BannerBean;
 import com.tntlinking.tntdev.ui.dialog.AppUpdateDialog;
+import com.tntlinking.tntdev.ui.fragment.OneFragment;
+import com.tntlinking.tntdev.ui.fragment.TwoFragment;
 import com.tntlinking.tntdev.widget.MyListView;
 
 import java.util.ArrayList;
 import java.util.List;
 
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
 import androidx.viewpager.widget.PagerAdapter;
 import androidx.viewpager.widget.ViewPager;
+import androidx.viewpager2.adapter.FragmentStateAdapter;
+import androidx.viewpager2.widget.ViewPager2;
 
 /**
  * 工作主页面
@@ -88,6 +91,8 @@ public final class HomeStatusActivity extends AppActivity {
     private ServiceProjectAdapter mServiceAdapter;
     private HistoryProjectAdapter mHistoryAdapter;
     String name = SPUtils.getInstance().getString(AppConfig.DEVELOP_NAME, "朋友");
+    private String[] titles = {"单选", "多选"};
+    private List<Fragment> fragmentList = new ArrayList<>();
 
     @Override
     protected int getLayoutId() {
@@ -227,7 +232,32 @@ public final class HomeStatusActivity extends AppActivity {
                 }
             }
         });
+//造数据
+        fragmentList.add(new OneFragment());
+        fragmentList.add(new TwoFragment());
+        TabLayout tabs = findViewById(R.id.tab_position);
+        ViewPager2 viewPager = findViewById(R.id.vp_position);
+        viewPager.setAdapter(new FragmentStateAdapter(this) {
+            @NonNull
+            @Override
+            public Fragment createFragment(int position) {
+                return fragmentList.get(position);
+            }
 
+            @Override
+            public int getItemCount() {
+                return fragmentList.size();
+            }
+        });
+        viewPager.setOffscreenPageLimit(2);
+        TabLayoutMediator tabLayoutMediator = new TabLayoutMediator(tabs, viewPager, new TabLayoutMediator.TabConfigurationStrategy() {
+            @Override
+            public void onConfigureTab(@NonNull TabLayout.Tab tab, int position) {
+                tab.setText(titles[position]);
+            }
+        });
+        //这句话很重要
+        tabLayoutMediator.attach();
     }
 
 
---
+++ app/src/main/java/com/tntlinking/tntdev/ui/fragment/OneFragment.java
@@ -0,0 +1,6 @@
+package com.tntlinking.tntdev.ui.fragment;
+
+import androidx.fragment.app.Fragment;
+
+public class OneFragment extends Fragment {
+}
